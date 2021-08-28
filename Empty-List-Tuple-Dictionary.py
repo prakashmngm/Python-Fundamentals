5e10cd2b182493f9a6861ff41a87ce4774cca1d7
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:25:25 -0400

    Add files via upload

---
+++ Empty-List-Tuple-Dictionary.py
@@ -0,0 +1,12 @@
+'''
+3.a)Create an empty list,tuple,dictionary and print its type.
+
+'''
+
+lst = []
+print(type(lst))
+tuple1 = ()
+print(type(tuple1))
+dictionary = {}
+print(type(dictionary))
+
