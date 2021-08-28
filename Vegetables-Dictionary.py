6ef44e2a5d3a40495aa0fd3fc5c9ae279e445fe2
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:27:19 -0400

    Add files via upload

---
+++ Vegetables-Dictionary.py
@@ -0,0 +1,15 @@
+'''
+3.d)Create a list of 5 vegetables names.
+Print it.
+
+3.e)Create a dict with 5 vegetables as keys, and amount to be purchased in kgs as values.
+
+'''
+
+veg = ['tomato','potato','carrot','onion','chilli']
+print(veg)
+veg1 = ('tomato','potato','carrot','onion','chilli')
+print(veg1)
+veg_price = {'tomato':5,'potato':3,'carrot':2,'onion':4,'chilli':7}
+print(veg_price)
+
