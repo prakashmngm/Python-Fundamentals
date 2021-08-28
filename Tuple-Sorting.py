9f87b0a5442e75b9156630b6b74eafddb95c7b36
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:33:28 -0400

    Add files via upload

---
+++ Tuple-Sorting.py
@@ -0,0 +1,13 @@
+
+'''
+
+create a tuple of 3 +ve and 3 -ve integers
+
+'''
+
+integers = (-2,-5,3,-8,5,9)
+print(max(integers))
+print(min(integers))
+print(sorted(integers))
+print(sorted(integers,reverse = True))
+
