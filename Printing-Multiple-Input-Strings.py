1b31f6d191d42bff1e078b9e32fbeac8dd1d4cf7
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:16:36 -0400

    Add files via upload

---
+++ Printing-Multiple-Input-Strings.py
@@ -0,0 +1,17 @@
+'''
+(2.b)Take input 3 strings from user. Print it back on screen.
+Enter String#1 : 
+Enter String#2 : 
+Enter String#3 : 
+You have entered : 
+
+
+'''
+
+myText1 = input("Enter String1: ")
+myText2 = input("Enter String2: ")
+myText3 = input("Enter String3: ")
+print("You have entered :",myText1,myText2,myText3)
+
+'''
+
