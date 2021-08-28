4f602bbecb4082858c60bdc486505731b6ea7fa6
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:07:29 -0400

    Add files via upload

---
+++ Hello-World.py
@@ -0,0 +1,15 @@
+1.a) print("Hello World")
+
+1.b)Now modify above program to print the following : 
+Hello World!
+My name is XYZ.
+I live in India.
+
+'''
+
+print("Hello World")
+print("My name is Surya.")
+print("I live in India.")
+
+'''
+
