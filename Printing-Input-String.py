5f651a7aaf601ce7c0e1c6b74e3a3ad1f09f3de5
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:14:59 -0400

    Add files via upload

---
+++ Printing-Input-String.py
@@ -0,0 +1,11 @@
+'''
+(2.a)Take input any string from user. Print it back on screen.
+Enter any string : 
+You have entered : 
+
+
+'''
+
+myText = input("Enter any string :")
+print("You have entered :"+myText)
+
