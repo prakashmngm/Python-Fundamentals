f1d665056d6d1f17dd21af95143f4846f9fc805e
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:32:12 -0400

    Add files via upload

---
+++ Creating_Tuple.py
@@ -0,0 +1,21 @@
+'''
+
+create a tuple of 5 colors
+
+'''
+
+colors = ('white','red','blue','green','black')
+print(colors)
+
+print(type(colors))
+
+print(colors[0])
+
+print(colors[-1])
+
+print(colors[2])
+
+print(colors.index('green'))
+
+print(len(colors))
+
