f2fa7db172cc3b1d8d36eb3f628b030cee9e9cf3
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:21:18 -0400

    Add files via upload

---
+++ Assigning-Variable-With-Diff-Datatypes.py
@@ -0,0 +1,44 @@
+'''
+(3.a)Initialize a variable with name - ‘counter’
+Assign value 3 to it. 
+Do : counter = counter + 10. 
+Print the value of counter.
+
+(3.b)Create a new variable : newCounter = counter + 10
+Print the value of newCounter.
+
+(3.c)Add this line to above prog :  counter = “My name is Lokesh”
+Print the value of counter.
+
+(3.d)Add this line to above prog : counter = 123.456
+Print the value of counter.
+
+(3.e)Add this line to the above prog : counter = True
+Print the value of counter.
+
+(3.f)Put the statement - print( type(counter) )
+After each of above statements. Observe the result.
+
+'''
+
+counter = 3
+counter = counter+10
+print(counter)
+print(type(counter))
+
+newCounter = counter+10
+print(newCounter)
+print(type(counter))
+
+counter = 'My name is Lokesh'
+print(counter)
+print(type(counter))
+
+counter = 123.456
+print(counter)
+print(type(counter))
+
+counter = True
+print(counter)
+print(type(counter))
+
