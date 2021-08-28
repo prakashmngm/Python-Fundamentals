f0490620d916b1a7964ecb1d9e0e533d002f232e
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:18:52 -0400

    Add files via upload

---
+++ Printing_Type_Of_The_Variable.py
@@ -0,0 +1,14 @@
+'''
+
+(2.c)Take input as the age of user.
+Print its type.
+
+
+'''
+
+age1 = input("Enter age1 : ")
+age2 = input("Enter age2 : ")
+print(type(age1))
+print(type(age2))
+print(age1+age2)
+
