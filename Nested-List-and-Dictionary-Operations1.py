ca62fe8dffd08e9159c1855ace88aa5f28c7e349
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:45:34 -0400

    Add files via upload

---
+++ Nested-List-and-Dictionary-Operations1.py
@@ -0,0 +1,24 @@
+'''
+
+**(1) Nested List & Nested Dict - Exercises
+
+'''
+
+myList = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+
+print(myList[2][0])
+
+print(myList[-1])
+
+print(myList[2][1][2][1])
+
+print(myList[2][2])
+
+print(myList[-1])
+
+print(myList[2][-1])
+
+print(myList[2][1][-1])
+
+print(myList[2][1][2][-1])
+
