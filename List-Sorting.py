2a6747fb0c67c57f5240e3e3b7f9d3fd63d1f3ec
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:30:44 -0400

    Add files via upload

---
+++ List-Sorting.py
@@ -0,0 +1,12 @@
+'''
+create a list with 3 positive and 3 negitive integers
+
+'''
+
+integers = [0,1,5,-2,-1,-3]
+print(min(integers))
+print(max(integers))
+print(sorted(integers))
+integers.sort(reverse = True)
+print(integers)
+
