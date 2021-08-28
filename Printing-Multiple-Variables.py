e1babd5311ca3553b413021f4dfc8f0663977c16
prakashmngm <mangam.suryaprakash@gmail.com>
2021-08-28 08:12:08 -0400

    Add files via upload

---
+++ Printing-Multiple-Variables.py
@@ -0,0 +1,19 @@
+'''
+(1.d) Now assign three texts from <1.b> to three different variables. And print them.
+
+(1.b)Now modify above program to print the following : 
+Hello World!
+My name is XYZ.
+I live in India.
+
+
+'''
+
+myText1 = "Hello World"
+myText2 = "My name is XYZ."
+myText3 = "I live in India."
+print(myText1,myText2,myText3)
+print(myText1)
+print(myText2)
+print(myText3)
+
